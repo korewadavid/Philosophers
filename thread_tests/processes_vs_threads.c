048e1b0cfbdf1c9abc945fc33616e45ec68b4a3b
korewadavid <korewadavid@gmail.com>
2023-09-27 16:34:08 +0200

    add threads ex2

---
+++ thread_tests/processes_vs_threads.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+
+#include <sys/wait.h>
+#include <sys/types.h>
+
+// processes: duplicate the calling process using fork()
+
+// threads

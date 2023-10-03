f4780b624f8e44aac132a7b79ebb60e86f572e57
korewadavid <korewadavid@gmail.com>
2023-10-03 17:18:21 +0200

    added input checking

---
+++ src/philo.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: damendez <damendez@student.42barcelona.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/03 16:00:36 by damendez          #+#    #+#             */
+/*   Updated: 2023/10/03 16:00:36 by damendez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+# include <pthread.h> // threads library
+# include <unistd.h> // write()
+# include <stdio.h> //stdin/stdout/printf
+# include <sys/time.h>
+
+# define MAX_NUM_PHILO 200
+
+#endif
---
+++ thread_tests/race_conditions.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+
+void* routine() {
+
+}
+
+int main() {
+    
+}

7430f19205230bfef8311b89fdcbb19b1910cfef
korewadavid <korewadavid@gmail.com>
2023-11-15 16:19:45 +0100

    added notes file

--- practice/threads_prac.c
+++ practice/threads_prac.c
@@ -5,12 +5,18 @@ void	*monitor(void *data_pointer) // Why is the monitor function and parameter o
 
 }
 
+// supervises a single routine thread 
+void	*supervisor(void *philo_pointer)
+{
+
+}
+
 // routine for each philo
 void	*routine(void *philo_pointer)
 {
 	// Cast the philo_pointer to a t_philo struct named philo (why?)
 	
-	// Calculate when current philo is supposed to die (time_to_die) and save in current philo struct
+	// Calculate when current philo is supposed to die (time_to_die) and save in current philo struct, this will be used to keep in mind when a philo should die
 	
 	// Create new thread that supervises current philos time_to_die and eat_count, checking for error
 	
@@ -23,12 +29,14 @@ void	*routine(void *philo_pointer)
 int		thread_init(t_data *data)
 {
 	// Make thread id buffer for monitor thread
+	pthread_t mt;
 
 	// Get current time as reference point for the starting time of program
+	data->start_time = get_time();
 
 	// If user gave a meals_nb create a "monitor" thread, return error on failure
-
-	// Create philosopher threads, return defined error on failure, in loop sleep between thread creation so their not all made at the same time
+	if (data->meals_nb > 0)
+		if (pthread_create(&mt, NULL, &monitor, ))
 	
 	// Wait for each philosopher thread to finish
 	

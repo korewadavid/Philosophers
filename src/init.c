c05a6e20b810e7a425a9639e09d08b2b1ffc31ae
korewadavid <korewadavid@gmail.com>
2023-11-27 16:52:25 +0100

    finished safe_functions

--- src/init.c
+++ src/init.c
@@ -6,20 +6,32 @@
 /*   By: damendez <damendez@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 18:04:39 by damendez          #+#    #+#             */
-/*   Updated: 2023/11/22 16:02:45 by damendez         ###   ########.fr       */
+/*   Updated: 2023/11/27 16:52:01 by damendez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-/*
- * 1. Set up remainding variables from t_data other than input variables from parsing
- *
-*/
+static void	philo_init(t_data *data)
+{
+	int	i;
+
+	i = -1;
+	while (++i < da)
+}
 
 void	init_data(t_data *data)
 {
+	int	i;
+
+	i == -1;
 	data->end_program = false;
-	data->philos = safe_malloc(table->philo_nbr);
-	
+	data->philos = safe_malloc(sizeof(t_philo) * data->philo_nbr);
+	data->forks = safe_malloc(sizeof(t_fork) * data->philo_nbr);
+	while (++i < data->philo_nbr)
+	{
+		safe_mutex_handle(&data->forks[i].fork, INIT);
+		data->forks[i].fork_id = i;
+	}
+	philo_init(data) //TO-DO
 }
--- src/safe_functions.c
+++ src/safe_functions.c
@@ -6,14 +6,14 @@
 /*   By: damendez <damendez@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 17:20:57 by damendez          #+#    #+#             */
-/*   Updated: 2023/11/23 17:20:57 by damendez         ###   ########.fr       */
+/*   Updated: 2023/11/27 15:47:03 by damendez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
 /*
-    Functions with embedded controls on return
+ *	Functions with embedded checks to errors	
 */
 
 void    *safe_malloc(size_t bytes)
@@ -26,13 +26,11 @@ void    *safe_malloc(size_t bytes)
     return (ret);
 }
 
-// *** Mutex Safe ***
 /*
-    init
-    destoy
-    lock
-    unlock
-*/
+ *	Handling Errors:
+ *	On success, mutex and thread functions return 0
+ *	else, an error number (errno.h) is returned indicating the error
+*/ 
 
 static void handle_mutex_error(int status, t_opcode opcode)
 {
@@ -43,7 +41,32 @@ static void handle_mutex_error(int status, t_opcode opcode)
     else if (status == EINVAL && opcode == INIT)
         error_exit("The value specified by attr is invalid.");
     else if (status == EDEADLK)
-        error_exit("")
+        error_exit("The current thread already owns the mutex.");
+	else if (status == EPERM)
+		error_exit("The current thread does not hold a lock on mutex.");
+	else if (status == ENOMEM)
+		error_exit("Insufficient memory exists to initialize the mutex.");
+	else if (status == EBUSY)
+		error_exit("Mutex is locked.")
+}
+
+static void handle_thread_error(int status, t_opcode opcode)
+{
+	if (status == 0)
+		return ;
+	if (status == EAGAIN)
+		error_exit("Insufficient resources to create another thread.");
+	else if (status == EPERM)
+		error_exit("The caller does not have appropiate permission.");
+	else if (status == EINVAL && opcode == CREATE)
+		error_exit("The value specified by attr is invalid.");
+	else if (status == EINVAL && (opcode == JOIN || opcode == DETACH))
+		error_exit("The value specified by thread is not joinable.");
+	else if (status == ESRCH)
+		error_exit("No thread with the ID specified could be found");
+	else if (status == EDEADLK)
+		error_exit("A deadlock was detected (e.g., two threads tried to join
+              with each other); or thread specifies the calling thread.");
 }
 
 void    safe_mutex_handle(pthread_mutex_t *mutex, t_opcode opcode)
@@ -57,5 +80,17 @@ void    safe_mutex_handle(pthread_mutex_t *mutex, t_opcode opcode)
     else if (opcode == DESTROY)
         handle_mutex_error(pthread_mutex_destroy(mutex), opcode);
     else
-        error_exit(OP_MUTEX_ERR);
+        error_exit("Invalid opcode for mutex_handle, use <LOCK> <UNLOCK> <INIT> <DESTROY>");
+}
+
+void	safe_thread_handle(pthread_t *thread, void *(*start_routine)(void *), void *arg, t_opcode opcode)
+{
+	if (opcode == CREATE)
+		handle_thread_error(pthread_create(thread, NULL, start_routine, arg), opcode);
+	else if (opcode == JOIN)
+		handle_thread_error(pthread_join(*thread, NULL), opcode);
+	else if (opcode == DETACH)
+		handle_thread_error(pthread_detach(*thread), opcode);
+	else
+		error_exit("Invalid opcode for thread_handle, use <CREATE> <JOIN> <DETACH>");
 }

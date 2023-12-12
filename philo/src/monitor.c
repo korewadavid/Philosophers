21460ef177a20023e89092da11d929ca24445779
korewadavid <korewadavid@gmail.com>
2023-12-12 15:26:44 +0100

    finished philo_died

--- philo/src/monitor.c
+++ philo/src/monitor.c
@@ -12,14 +12,44 @@
 
 #include "philo.h"
 
-static bool	philo_died(t_philo *philo)
+/*
+ * for each philo, if time has passed since their last meal time and that time is >= die_t
+ *, and program hasnt been marked as finished, mark it as finished and print that the current philo died
+*/
+static bool	philo_died(t_data *data)
 {
+	int	i;
+	unsigned long	elapsed;
+	unsigned long	t1;
+	unsigned long	t2;
 
+	i = -1;
+	while (++i < data->philo_nb)
+	{
+		t2 = get_time();
+		t1 = data->philo[i].last_meal_t;
+		elapsed = t2 - t1;
+		if (t1 < t2 && (elapsed >= data->die_t) && (data->finish != true))
+		{
+			safe_mutex_handle(&data->m_finish, LOCK);
+			data->finish = true;
+			safe_mutex_handle(&data->philo[i].m_eating, LOCK);
+			if (data->philo[i].eating == 0)
+			{
+				safe_mutex_handle(&data->m_print, LOCK);
+				ft_print_died(&data->philo[i], "died");
+				safe_mutex_handle(&data->m_print, UNLOCK);
+			}
+			safe_mutex_handle(&data->philo[i].m_eating, UNLOCK);
+			return (true);
+		}
+	}
+	return (false);
 }
 
 static bool	philos_finished(t_philo *philo)
 {
-	
+
 }
 
 /*
